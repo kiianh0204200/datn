dace7c1af2394bd612b8e84af1f47a261dc6839d
binh le <accfreefire2504@gmail.com>
2024-08-23 23:56:15 +0700

    uodate Register

--- app/Http/Controllers/Admin/RegisterController.php
+++ app/Http/Controllers/Admin/RegisterController.php
@@ -40,21 +40,3 @@ class RegisterController extends Controller
         return redirect(route('admin.home'));
     }
 }
-public function store(RegisterRequest $request)
-{
-    $data = $request->safe()->all();
-
-    $user = User::create([
-        'name' => $data['name'],
-        'email' => $data['email'],
-        'password' => bcrypt($data['password']),
-    ]);
-
-    $role = Role::findByName('employee');
-
-    $user->assignRole($role);
-
-    Auth::login($user);
-
-    return redirect(route('admin.home'));
-}
