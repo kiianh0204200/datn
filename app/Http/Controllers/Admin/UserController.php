6dd7e0bb4f9039a152fc17292c1ad1c62cec7d7a
Phạm Anh Tuấn <anhptph30273@fpt.edu.vn>
2024-08-24 17:21:56 +0700

    update user

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -109,3 +109,40 @@ public function logout(Request $request)
 
     return redirect(route('auth.login'));
 }
+public function logout(Request $request)
+{
+    Auth::logout();
+    $request->session()->invalidate();
+
+    $request->session()->regenerateToken();
+
+    return redirect(route('auth.login'));
+}
+public function logout(Request $request)
+{
+    Auth::logout();
+    $request->session()->invalidate();
+
+    $request->session()->regenerateToken();
+
+    return redirect(route('auth.login'));
+}
+public function logout(Request $request)
+{
+    Auth::logout();
+    $request->session()->invalidate();
+
+    $request->session()->regenerateToken();
+
+    return redirect(route('auth.login'));
+}
+public function logout(Request $request)
+{
+    Auth::logout();
+    $request->session()->invalidate();
+
+    $request->session()->regenerateToken();
+
+    return redirect(route('auth.login'));
+}
+
