b851aac61f07540329dc5c01d7a2bad2cf6256c4
cuong-tn <haicuong0979@gmail.com>
2024-08-24 18:26:40 +0700

    new file

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,35 +4,49 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
     use HasFactory;
 
-    protected $table = 'orders';
+    const STATUS_ORDER = [
+        'pending' => 'Chờ xác nhận',
+        'confirmed' => 'Đã xác nhận',
+        'preparing_goods' => 'Đang chuẩn bị hàng',
+        'shipping' => 'Đang vận chuyển',
+        'delivered' => 'Đã giao hàng',
+        'canceled' => 'Đơn hàng đã bị hủy',
+    ];
+
+    const STATUS_PAYMENT = [
+        'unpaid' => 'Chưa thanh toán',
+        'paid' => 'Đã thanh toán',
+    ];
+
+    const STATUS_ORDER_PENDING = 'pending';
+    const STATUS_ORDER_CONFIRMED = 'confirmed';
+    const STATUS_ORDER_PREPARING_GOODS = 'preparing_goods';
+    const STATUS_ORDER_SHIPPING = 'shipping';
+    const STATUS_ORDER_DELIVERED = 'delivered';
+    const STATUS_ORDER_CANCELED = 'canceled';
+    const STATUS_PAYMENT_UNPAID = 'unpaid';
+    const STATUS_PAYMENT_PAID = 'paid';
 
     protected $fillable = [
         'user_id',
-        'order_id',
-        'total',
-        'payment_method',
-        'payment_status',
-        'order_status',
-        'payment_id',
-        'address',
-        'address_2',
-        'notes',
+        'user_name',
+        'user_email',
+        'user_phone',
+        'user_address',
+        'user_note',
+        'is_ship_user_same_user',
+        'ship_user_name',
+        'ship_user_email',
+        'ship_user_phone',
+        'ship_user_address',
+        'ship_user_note',
+        'status_order',
+        'status_payment',
+        'total_price',
     ];
-
-    public function user(): BelongsTo
-    {
-        return $this->belongsTo(User::class);
-    }
-
-    public function orderItems(): HasMany
-    {
-        return $this->hasMany(OrderDetail::class, 'order_id', 'id');
-    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,87 +4,56 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
     use HasFactory;
 
-    protected $table = 'products';
-
     protected $fillable = [
-        'brand_id',
-        'product_category_id',
+        'catelogue_id',
         'name',
-        'subtitle',
-        'description',
-        'thumbnail',
         'slug',
-        'price',
-        'discount',
-        'condition',
-        'is_active',
         'sku',
+        'img_thumbnail',
+        'price_regular',
+        'price_sale',
+        'description',
+        'content',
+        'material',
+        'user_manual',
+        'views',
+        'is_active',
+        'is_hot_deal',
+        'is_good_deal',
+        'is_new',
+        'is_show_home',
     ];
 
-    protected $appends = [
-        'thumbnail_url',
+    protected $casts = [
+        'is_active' => 'boolean',
+        'is_hot_deal' => 'boolean',
+        'is_good_deal' => 'boolean',
+        'is_new' => 'boolean',
+        'is_show_home' => 'boolean',
     ];
 
-    public function getThumbnailUrlAttribute(): string
+    public function catelogue()
     {
-        return asset('uploads/products/' . $this->thumbnail);
+        return $this->belongsTo(Catelogue::class);
     }
 
-    public function brand(): BelongsTo
+    public function tags()
     {
-        return $this->belongsTo(Brand::class, 'brand_id', 'id');
+        return $this->belongsToMany(Tag::class);
     }
 
-    public function productCategory(): BelongsTo
+    public function galleries()
     {
-        return $this->belongsTo(ProductCategory::class, 'product_category_id', 'id');
+        return $this->hasMany(ProductGallery::class);
     }
 
-    public function productOptions(): BelongsTo
+    public function variants()
     {
-        return $this->belongsTo(ProductOption::class, 'product_option_id', 'id');
-    }
-
-    public function color(): HasMany
-    {
-        return $this->hasMany(ProductOptionValue::class, 'product_id', 'id');
-    }
-
-    public function size(): HasMany
-    {
-        return $this->hasMany(ProductOptionValue::class, 'product_id', 'id');
-    }
-
-    public function productOptionValueColor(): BelongsToMany
-    {
-        return $this->belongsToMany(ProductOption::class, 'product_option_values', 'product_id', 'color_id')->distinct('color_id');
-    }
-
-    public function productOptionValueSize(): BelongsToMany
-    {
-        return $this->belongsToMany(ProductOption::class, 'product_option_values', 'product_id', 'size_id');
-    }
-
-    public function productOptionValues(): HasMany
-    {
-        return $this->hasMany(ProductOptionValue::class, 'product_id', 'id');
-    }
-
-    public function productImages(): HasMany
-    {
-        return $this->hasMany(ProductImage::class, 'product_id', 'id');
-    }
-
-    public function comments(): HasMany
-    {
-        return $this->hasMany(ProductComment::class, 'product_id', 'id');
+        return $this->hasMany(ProductVariant::class);
     }
 }
