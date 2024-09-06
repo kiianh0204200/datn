5c0efeb7cb0edc993a2fdece987b3bf260d4d9a8
binh le <accfreefire2504@gmail.com>
2024-09-06 14:06:46 +0700

    ....

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -80,7 +80,7 @@ class HomeController extends Controller
         return Order::query()
             ->select('order_status', DB::raw('COUNT(*) as count'))
             ->whereBetween('created_at', [$start, $end])
-            ->groupBy('order_status')
+        
            
     }
 }
